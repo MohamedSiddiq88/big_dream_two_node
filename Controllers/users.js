6d008498ed86e8c4ca011050c79af539156d45ad
MohamedSiddiq88 <diddiq88@gmail.com^c>
2024-09-04 15:27:57 +0530

    db

--- Controllers/users.js
+++ Controllers/users.js
@@ -3,14 +3,14 @@ import  jwt  from "jsonwebtoken";
 
 export function  addUsers(userInfo){
     return client
-    .db("project1")
+    .db("dream-big")
     .collection("users")
     .insertOne(userInfo)
 }
 
 export function  getUser(userphoneNumber){
     return client
-    .db("project1")
+    .db("dream-big")
     .collection("users")
     .findOne({phoneNumber:userphoneNumber})
 }
@@ -22,18 +22,18 @@ export function  generateJwtToken(id){
 export function addRandomString(randomString,phoneNumber){
     
     const existMail = client
-    .db("project1")
+    .db("dream-big")
     .collection("randomstring")
     .findOne({phoneNumber:phoneNumber})
 
     if(existMail){
         return client
-        .db("project1")
+        .db("dream-big")
         .collection("randomstring")
         .findOneAndUpdate({phoneNumber:phoneNumber},{$set:{randomString:randomString}})   
     }else{
         return client
-        .db("project1")
+        .db("dream-big")
         .collection("randomstring")
         .insertOne({
             randomString: randomString,
@@ -46,21 +46,21 @@ export function addRandomString(randomString,phoneNumber){
 
 export function  getRandom(randomString){
     return client
-    .db("project1")
+    .db("dream-big")
     .collection("randomstring")
     .findOne({randomString: randomString})
 }
 
 export function deleteRandomString(randomString){
      client
-        .db("project1")
+        .db("dream-big")
         .collection("randomstring")
         .deleteOne({ randomString: randomString });
 }
 
 export function updatePassword(phoneNumber,password){
     return client
-        .db("project1")
+        .db("dream-big")
         .collection("users")
         .updateOne({ phoneNumber: phoneNumber }, { $set: { password: password } });
 }
